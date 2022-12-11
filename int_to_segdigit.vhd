88eb594d9189ee7b3f5a02b222d84968032dde74
Murad Arslaner <marslan1@stevens.edu>
2022-12-11 15:47:08 -0500

    dataflow test

--- int_to_segdigit.vhd
+++ int_to_segdigit.vhd
@@ -10,6 +10,7 @@ END int_to_segdigit;
 
 ARCHITECTURE Behavioral OF int_to_segdigit IS
 BEGIN
+-- Perfectly working behavioral
 	conversion : PROCESS (int)
 	BEGIN
 		CASE int IS
@@ -23,7 +24,20 @@ BEGIN
 			WHEN 7 => segdigit <= "0001111";
 			WHEN 8 => segdigit <= "0000000";
 			WHEN 9 => segdigit <= "0000100";
-			WHEN OTHERS => segdigit <= "1111111";
+			WHEN OTHERS => NULL;
 		END CASE;
 	END PROCESS;
+	
+-- Slightly buggy dataflow
+--	segdigit <= "0000001" WHEN int = 0 ELSE
+--	"1001111" WHEN int = 1 ELSE
+--	"0010010" WHEN int = 2 ELSE
+--	"0000110" WHEN int = 3 ELSE
+--	"1001100" WHEN int = 4 ELSE
+--	"0100100" WHEN int = 5 ELSE
+--	"0100000" WHEN int = 6 ELSE
+--	"0001111" WHEN int = 7 ELSE
+--	"0000000" WHEN int = 8 ELSE
+--	"0000100" WHEN int = 9 ELSE
+--	"1111111";
 END Behavioral;
